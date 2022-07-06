c3b27856f2a0fe2908188322f8bc934a77278b53
Alyahehehe <108801766+Alyahehehe@users.noreply.github.com>
2022-07-06 18:16:07 +0700

    config.js

--- config.js
+++ config.js
@@ -47,7 +47,7 @@ global.urlnya = "https://www.instagram.com/aguzfamilia"
 
 //============= callButtons =============//
 global.dtc = 'ᴄᴀʟʟ ᴏᴡɴᴇʀ'
-global.phn = '+62 813-2017-0984'
+global.phn = '+62 818-0262-6072'
 
 //============= Games ================//
 global.benar = '_*Benar✅*_'

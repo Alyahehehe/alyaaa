b6f9380751ae23a37b141783872b71655c46d6a2
Alyahehehe <108801766+Alyahehehe@users.noreply.github.com>
2022-07-06 18:10:36 +0700

    donasi.js

--- plugins/donasi.js
+++ plugins/donasi.js
@@ -2,11 +2,11 @@ const qrku = "https://i.ibb.co/MnbqZ0N/qr-Aguz-Familia-07-06-22-1654614928.png"
 
 let handler = async (m, { conn, usedPrefix }) => conn.sendButtonImg(m.chat, qrku, `
 ╭─「 Donasi • Dana 」
-│ • Telkomsel [081320170984]
-│ • Dana  [081320170984]
+│ • tri [0895324080019]
+│ • Dana  [0895324080019]
 ╰────
 ╭─「 *NOTE* 」
-│ > Ingin donasi? Wa.me/6281320170984
+│ > Ingin donasi? Wa.me/6281802626072
 │ _Hasil donasi akan digunakan buat sewa_
 │ _atau beli *RDP/VPS* agar bot bisa jalan_
 │ _24jam tanpa kendala_
